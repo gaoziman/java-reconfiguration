a1b6c6d057e28bca926cfb20104d12e28389a45b
“gaoziman” <2942894660@qq.com>
2023-11-03 14:51:18 +0800

    BIO聊天案例

---
+++ chapter06_socket/src/main/java/org/javatop/chat/Constants.java
@@ -0,0 +1,14 @@
+package org.javatop.chat;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-11-03 14:26
+ * @description :
+ */
+public class Constants {
+
+    /** 常量 */
+    public static final int PORT = 7778 ;
+    public static final String SPILIT = "23" ;
+}
