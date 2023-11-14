452c9d327b0913b49b9541e8a1de1c4de183126c
gaoziman <2942894660@qq.com>
2023-11-14 23:20:29 +0800

    添加测试类

---
+++ chapter02_Arrays/src/main/java/com/Leo/array/Leo01/Test.java
@@ -0,0 +1,20 @@
+package com.Leo.array.Leo01;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-11-14 23:18
+ * @description :
+ */
+public class Test {
+
+    /**
+     *  用于测试:
+     */
+    @org.junit.jupiter.api.Test
+    public void test01() {
+        System.out.println("true = " + true);
+    }
+
+
+}
